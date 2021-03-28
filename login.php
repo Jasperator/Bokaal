dd9e4524e31c447f4675ead8e0b89d28df58d82c
jasper <jasper@youreka.be>
2021-03-28 11:56:36 +0200

    Start aan database configuratie, mappenstructuur en login/register

---
+++ login.php
@@ -0,0 +1,106 @@
+<?php
+
+include_once(__DIR__ . "/bootstrap.include.php");
+
+//Detect submit
+if (!empty($_POST)) {
+
+  //Put fields in variables
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+
+  if (!empty($email) && !empty($password)) {
+    //If both fields are filled in, check if the login is correct
+
+    if (classes\User::checkPassword($email, $password)) {
+      $user = new classes\User($email);
+
+      if ($_POST['captcha'] == $_SESSION['digit']) {
+       $_SESSION['user'] = $email;
+          header("Location: index.php");
+       
+      } else {
+        $error = "Wrong Captcha";
+      }
+    } else {
+      $error = "Sorry, we couldn't log you in.";
+    }
+  } else {
+
+    //If one of the fields is empty, generate an error
+    $error = "Email and password are required.";
+  }
+}
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="fonts/font-awesome-5/css/fontawesome-all.min.css">
+  <link rel="stylesheet" href="css/login.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400,700&display=swap">
+  <link rel="stylesheet" href="fonts/font-awesome-5/css/fontawesome-all.min.css">
+  <title>Bokaal: login</title>
+</head>
+
+<body>
+
+  <?php include_once("nav.include.php"); ?>
+
+  <div class="container-fluid">
+    <div class="row no-gutter">
+      <div class="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
+      <div class="col-md-8 col-lg-6">
+        <div class="login d-flex align-items-center py-5">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-9 col-lg-8 mx-auto">
+                <h3 class="login-heading mb-4">Welcome back!</h3>
+
+
+                <?php if (isset($error)) : ?>
+                  <div class="col-md-9 col-lg-8 mx-auto">
+                    <h3 class="login-heading mb-4" style="font-size: 15px; background-color:#F8D7DA; padding:10px; border-radius:10px;"><?php echo $error; ?></h3>
+                  </div>
+                <?php endif; ?>
+
+                <form action="" method="post">
+                  <div class="form-label-group">
+                    <input type="text" name="email" id="email" class="form-control" autofocus>
+                    <label for="inputEmail">Thomas More Email</label>
+                  </div>
+
+                  <div class="form-label-group">
+                    <input type="password" name="password" id="password" class="form-control">
+                    <label for="inputPassword">Password</label>
+                  </div>
+
+
+                  <p><img src="./captcha.php" width="120" height="30" alt="CAPTCHA">
+                    <input type="text" size="6" maxlength="5" name="captcha" value=""></p>
+                  <p><small>Copy the digits from the image into the box</small></p>
+
+                  <input type="submit" value="LOGIN" class="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2">
+
+                  <div class="text-center">
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.js"></script>
+
+</body>
+
+</html>
---
+++ nav.include.php
@@ -0,0 +1,65 @@
+<?php
+
+include_once(__DIR__ . "/bootstrap.include.php");
+
+//Put the pagename in a variable
+//PHP_SELF returns the path, basename shortens it to the filename
+$page = basename($_SERVER['PHP_SELF']);
+
+//Get the amount of unread messages
+if (!empty($_SESSION['user'])) {
+    $user = new classes\User($_SESSION['user']);
+    $unread_messages = $user->checkUnreadMessages();
+    $unread_amount = count($unread_messages);
+}
+
+?>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="index.php">Bokaal</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+
+            <!-- If there's no active session, show the login/signup links -->
+            <?php if (!empty($_SESSION['user'])) :
+                $user = new classes\User($_SESSION['user']);
+            ?>
+                <ul class="navbar-nav mr-auto">
+
+                    <!-- Mark a link as "active" according to the current page -->
+                    <li class="nav-item <?php if ($page == "index.php") : echo "active";
+                                        endif; ?>">
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
+
+                </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="profile.php"><i class="fas fa-user"></i>&nbsp&nbsp<?php echo htmlspecialchars($user->getFullname()); ?></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php">Log out</a>
+                    </li>
+                </ul>
+            <?php else : ?>
+
+                <!-- If there's an active session, make a new user and show the site navigation -->
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link <?php if ($page == "login.php") : echo "active";
+                                            endif; ?>" href="login.php">
+                            <i class="fas fa-user"></i> Log in</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php if ($page == "register.php") : echo "active";
+                                            endif; ?>" href="register.php">
+                            <i class="fas fa-user-plus"></i> Sign up</a>
+                    </li>
+                </ul>
+            <?php endif; ?>
+        </div>
+    </div>
+</nav>
