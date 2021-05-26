e67c88a083535582520a5109c3110255fd141513
Bo Docxk <bo.dockx@gmail.com>
2021-05-26 13:34:22 +0200

    push

--- index.php
+++ index.php
@@ -67,7 +67,7 @@ $sellers = $user->getSellersExceptUser();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/bootstrap.css">
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="../../css/style.css">
     <link rel="icon" type="image/svg" href=images/logo/favicon.png> <title>Bokaal | Home</title>
 </head>
 
@@ -75,104 +75,98 @@ $sellers = $user->getSellersExceptUser();
     <?php include_once("php/includes/nav.include.php");?>
     <div>
 
-    
-        <div >
+
+        <div>
             <h2 class="hoofdtitel">Home</h2>
         </div>
         <h3 class="titel-index">Favorieten</h3>
-        <ul id="all">
-            <?php
-            foreach ($favorites as $fav) : ?>
-            <li id="list">
-                <div class="itemId" data-id = "<?= htmlspecialchars($fav->id); ?>">
-                    <div>
-                        <div >
-                            <div >
-                                <div class="card" style="width: 18rem;">
-                                    <form action="" method="post">
-                                        <img class="card-img-top" src="./uploads/<?= htmlspecialchars($fav->profile_img); ?>" />
-                                </div>
+        <ul id="all-detail" class="row col-md-12">
+
+            <?php foreach ($favorites as $fav) : ?>
+                <div id="list-decoration" class="col-md-3">
+                    <div class="itemId" data-id="<?= htmlspecialchars($fav->id); ?>">
+                        <div class="container">
+                            <div class="card h-100" style="width: 18rem;">
+                                <form action="" method="post">
+                                    <img class="card-img-top" src="./uploads/<?= htmlspecialchars($fav->profile_img); ?>"
+                                        class="img-thumbnail border-0" />
+
+                                    <div id="card-body">
+                                        <h5 class="card-title"><?= htmlspecialchars($fav->fullname); ?></h5>
+                                        <p class="card-text"><?= htmlspecialchars($fav->location); ?></p>
+                                        <p class="card-text"><?= htmlspecialchars($fav->company);  ?></p>
+                                        <p class="card-text"><small class="text-muted"> Afstand:
+                                                <?= htmlspecialchars($fav->distance);  ?></small>
+                                        </p>
+                                    </div>
                             </div>
                         </div>
-
-                        <div id="card-body">
-                            <h5 class="card-title"><?= htmlspecialchars($fav->fullname); ?></h5>
-                            <p class="card-text"><?= htmlspecialchars($fav->location); ?></p>
-                            <p class="card-text"><?= htmlspecialchars($fav->company);  ?></p>
-                            <p class="card-text"> Afstand:
-                                <?= htmlspecialchars($fav->distance);  ?>
-                            </p>
-
-                            
-
-                            
-                        </div>
-                        <?php endforeach ?>
                     </div>
-            </li>
+                </div>
+            <?php endforeach ?>
+                            
         </ul>
         <div class="space-favor"></div>
-          <h3 class="titel-index"> Verkopers</h3>
+        <h3 class="titel-index"> Verkopers</h3>
 
         <ul id="all-detail" class="row col-md-12">
             <!--<div id="pauze"></div>-->
-          
+
             <?php foreach ($sellers as $seller) : ?>
             <div id="list-decoration" class="col-md-3">
+                <div class="itemId" data-id="<?= htmlspecialchars($seller->id); ?>">
+                    <div class="container">
+                        <div class="card h-100" style="width: 18rem;">
+                            <form action="" method="post">
+                                <img class="card-img-top" src="./uploads/<?= htmlspecialchars($seller->profile_img); ?>"
+                                    class="img-thumbnail border-0" />
 
-                <div class="itemId" data-id = "<?= htmlspecialchars($seller->id); ?>">
-                    <div  class="container">
-                                <div class="card h-100" style="width: 18rem;">
-                                    <form action="" method="post">
-                                        <img  class="card-img-top" src="./uploads/<?= htmlspecialchars($seller->profile_img); ?>"
-                                            class="img-thumbnail border-0" />
-                              
+                                <div class="card-body">
+                                    <h5 class="card-title"><?= htmlspecialchars($seller->fullname); ?></h5>
+                                    <p class="card-text"><?= htmlspecialchars($seller->location); ?></p>
+                                    <p class="card-text"><?= htmlspecialchars($seller->company);  ?></p>
+                                    <p class="card-text"><small class="text-muted">Afstand:
+                                            <?= htmlspecialchars($seller->distance);  ?></small>
+                                    </p>
+                                    <form id="favor" action="" method="post">
+                                        <div class="fav-but" class="form-group">
+                                            <button id="knop" type="submit" name="favorite-person" class="fav"
+                                                value="<?= htmlspecialchars($seller->id); ?>" name="fav"> <img
+                                                    class="favor-img" src="/images/icon/star.png" alt=""></button>
+                                        </div>
+                                    </form>
 
-                        <div class="card-body">
-                            <h5 class="card-title"><?= htmlspecialchars($seller->fullname); ?></h5>
-                            <p class="card-title"><?= htmlspecialchars($seller->location); ?></p>
-                            <p class="card-title"><?= htmlspecialchars($seller->company);  ?></p>
-                            <p class="card-title"><small class="text-muted">Afstand:
-                                <?= htmlspecialchars($seller->distance);  ?></small> 
-                            </p>
 
-
-                            <form id="favor" action="" method="post">
-
-                                <div class="fav-but" class="form-group">
-                                    <button id="knop" type="submit" name="favorite-person" class="fav"
-                                        value="<?= htmlspecialchars($seller->id); ?>" name="fav"> <img class="favor-img"
-                                            src="/images/icon/star.png" alt=""></button>
                                 </div>
-                            </form>
-
-
                         </div>
-                        </div>
-                    </div></div></div>
+                    </div>
+                </div>
+            </div>
 
-                        
 
-                        <?php endforeach ?>
+
+            <?php endforeach ?>
         </ul>
 
 
-    <div id="space"></div>
-    
+        <div id="space"></div>
 
 
 
-    <script>
-        document.querySelectorAll('.users').forEach(item => { item.addEventListener('click', function () {
 
-            window.location.href = `detailsUser.php?data-id=${this.getAttribute('data-id')}`
-        })})
-    </script>
+        <script>
+            document.querySelectorAll('.users').forEach(item => {
+                item.addEventListener('click', function () {
+
+                    window.location.href = `detailsUser.php?data-id=${this.getAttribute('data-id')}`
+                })
+            })
+        </script>
 
 
 
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.js"></script>
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.js"></script>
 </body>
 
 </html>
