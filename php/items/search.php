bfc42583e172bd3575d8d88254fd9c199f301f45
jasper <jasper@youreka.be>
2021-05-26 13:37:04 +0200

    bootstrap aan search toegevoegd

--- php/items/search.php
+++ php/items/search.php
@@ -44,6 +44,7 @@ if (!empty($_POST['searchName']) and (!empty($_POST['category']))) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/bootstrap.css">
     <link rel="icon" type="image/svg" href=../../images/logo/favicon.png> <link rel="stylesheet"
         href="../../css/style.css">
 
