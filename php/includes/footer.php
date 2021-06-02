1c0d75d60647184f12e5a29ce16b34b2df332cb8
YannickJ7 <yannick.joos@hotmail.be>
2021-06-02 17:50:33 +0200

    commit

--- php/includes/footer.php
+++ php/includes/footer.php
@@ -21,10 +21,7 @@
 
                      <div class="footerContact">
                          <h5><b>CONTACT</b></h5><a HREF="mailto:bokaal@bodockxdesign.be">bokaal@bodockxdesign.be</a>
-                         <span> <a href="https://www.facebook.com/Bokaal-school-project-100946261967222"><i id="social-media" class="fa fa-facebook">          </i></a></span> 
-                            <span> <a href="https://www.linkedin.com/in/bokaal-lab-3-4a8086203/%22%3E "><i id="social-media" class="fa fa-linkedin-square">   </i></a></span>
-                            <span> <a href="https://twitter.com/BokaalS"><i id="social-media" class="fa fa-twitter">           </i></a></span>
-                            <span> <a href="https://www.instagram.com/bokaal.schoolproject/"><i id="social-media" class="fa fa-instagram">         </i></a></span> 
+                         
                         </div>
                  </div>
              </div>
