769e4b2fbc6b35badd9a1dd7ce1a2fbf3c0fbcf6
YannickJ7 <yannick.joos@hotmail.be>
2021-06-02 17:26:29 +0200

    commit

--- php/includes/footer.php
+++ php/includes/footer.php
@@ -1,40 +1,31 @@
 
      <footer>
-         <div class="row my-5 justify-content-center py-5">
-             <div class="col-11">
-                 <div class="row ">
-                     <div class="col-xl-8 col-md-4 col-sm-4 col-12 a">
-                         <h3 class="text-muted mb-md-0 mb-5 bold-text"> <img style="max-width:15%;" src="/images/logo/LogoWhite.svg" alt=""></h3>
-                     </div>
-                     <div class="col-xl-2 col-md-4 col-sm-4 col-12">
-                         <h5 class="mb-3 mb-lg-4 bold-text "><b>MENU</b></h5>
+         <div>
+             <div>
+                 <div>
+                    
+                     <div class="footerMenu">
+                         <h5><b>MENU</b></h5>
 
-                         <ul class="list-unstyled">
-                             <li><a class="link-unstyled text-muted" href="/index.php">Home</a></li>
-                             <li><a class="link-unstyled text-muted" href="/php/items/search.php">Items</a></li>
-                             <li><a class="link-unstyled text-muted" href="/php/items/cart.php">winkelmandje</a></li>
-                             <li><a class="link-unstyled text-muted" href="/php/profile/favor.php">Favorieten</a></li>
+                         <ul>
+                             <li><a href="/index.php">Home</a></li>
+                             <li><a href="/php/items/search.php">Items</a></li>
+                             <li><a href="/php/items/cart.php">Winkelmandje</a></li>
+                             <li><a href="/php/profile/favor.php">Profiel</a></li>
                          </ul>
+                     </div>
+                         <div class="footerCenter">
+                <img class="footerLogo" src="../../images/Logo/LogoWhite.svg">
+                     </div>
+                 <div>
 
-                     </div>
-                     <div class="col-xl-2 col-md-4 col-sm-4 col-12">
-                         <h5 class="mb-3 mb-lg-4 text-muted bold-text mt-sm-0 mt-5"><b>ADRES</b></h5>
-                         <p class="mb-1">Bij jouw in de buurt</p>
-                         <p>Lokale boerderij</p>
-                     </div>
-                 </div>
-                 <div class="row ">
-                     <div class="col-xl-8 col-md-4 col-sm-4 col-auto align-self-end">
-                         <p class="social text-muted mb-0 pb-0 bold-text"> 
-                            <span> <a href="https://www.facebook.com/Bokaal-school-project-100946261967222"><i id="social-media" class="fa fa-facebook">          </i></a></span> 
+                     <div class="footerContact">
+                         <h5><b>CONTACT</b></h5><a HREF="mailto:bokaal@bodockxdesign.be">bokaal@bodockxdesign.be</a>
+                         <span> <a href="https://www.facebook.com/Bokaal-school-project-100946261967222"><i id="social-media" class="fa fa-facebook">          </i></a></span> 
                             <span> <a href="https://www.linkedin.com/in/bokaal-lab-3-4a8086203/%22%3E "><i id="social-media" class="fa fa-linkedin-square">   </i></a></span>
                             <span> <a href="https://twitter.com/BokaalS"><i id="social-media" class="fa fa-twitter">           </i></a></span>
                             <span> <a href="https://www.instagram.com/bokaal.schoolproject/"><i id="social-media" class="fa fa-instagram">         </i></a></span> 
-                        </p>
-                     </div>
-                     <div class="col-xl-2 col-md-4 col-sm-4 col-auto order-1 align-self-end ">
-                         <h5 class="mt-55 mt-2 text-muted bold-text"><b>CONTACT</b></h5><small><a HREF="mailto:bokaal@bodockxdesign.be">bokaal@bodockxdesign.be</a></small>
-                     </div>
+                        </div>
                  </div>
              </div>
          </div>
