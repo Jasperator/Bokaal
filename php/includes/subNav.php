395fc5b3bf8ea86259b84148eaba9a599c648025
YannickJ7 <yannick.joos@hotmail.be>
2021-06-03 15:38:26 +0200

    commit

--- php/includes/subNav.php
+++ php/includes/subNav.php
@@ -1,6 +1,6 @@
-<div class="icon-full-nav-profile" >
-    <a href="favor.php"><img class="icons" src="../../images/icon/star.png" alt=""></a>
-    <a href="chat.php"><img class="icons" src="../../images/icon/messenger.png" alt=""></a>
-    <a href="bought.php"><img class="icons" src="../../images/icon/dollar.png" alt=""></a>
-    <a href="settings.php"><img class="icons" src="../../images/icon/setting.png" alt=""></a>
+<div class="subMenu" >
+    <a href="favor.php"><img class="subIcon" src="../../images/icon/star.png" alt=""></a>
+    <a href="chat.php"><img class="subIcon" src="../../images/icon/messenger.png" alt=""></a>
+    <a href="bought.php"><img class="subIcon" src="../../images/icon/dollar.png" alt=""></a>
+    <a href="settings.php"><img class="subIcon" src="../../images/icon/setting.png" alt=""></a>
   </div>
