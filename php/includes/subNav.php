e2407895c6b0f9b8a80e9408ec76366e6ff63061
jasper <jasper@youreka.be>
2021-06-03 17:48:20 +0200

    subNav.php bijgewerkt, hover + active class

--- php/includes/subNav.php
+++ php/includes/subNav.php
@@ -12,8 +12,8 @@ $page = basename($_SERVER['PHP_SELF']);
 ?>
 
 <div class="subMenu" >
-    <a href="favor.php" <?php if ($page == "favor.php") : echo "active";?> class="activated" <?php endif; ?>><img class="subIcon" src="../../images/icon/star.png" alt=""></a>
-    <a href="chat.php" <?php if ($page == "chat.php") : echo "active";?> class="activated" <?php endif; ?>><img class="subIcon" src="../../images/icon/messenger.png" alt=""></a>
-    <a href="bought.php" <?php if ($page == "bought.php") : echo "active";?> class="activated" <?php endif; ?>><img class="subIcon" src="../../images/icon/dollar.png" alt=""></a>
-    <a href="settings.php" <?php if ($page == "settings.php") : echo "active";?> class="activated" <?php endif; ?>><img class="subIcon" src="../../images/icon/setting.png" alt=""></a>
+    <a href="favor.php"><img  <?php if ($page == "favor.php") : echo "active";?> class="activated subIcon" <?php endif; ?> class="subIcon" src="../../images/icon/star.png" alt=""></a>
+    <a href="chat.php"><img <?php if ($page == "chat.php") : echo "active";?> class="activated subIcon" <?php endif; ?> class="subIcon" src="../../images/icon/messenger.png" alt=""></a>
+    <a href="bought.php"><img  <?php if ($page == "bought.php") : echo "active";?> class="activated subIcon" <?php endif; ?> class="subIcon" src="../../images/icon/dollar.png" alt=""></a>
+    <a href="settings.php"><img  <?php if ($page == "settings.php") : echo "active";?> class="activated subIcon" <?php endif; ?> class="subIcon" src="../../images/icon/setting.png" alt=""></a>
   </div>
